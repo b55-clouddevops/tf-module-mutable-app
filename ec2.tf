b2e9f2a48155d9850037bc9e635523883f6cf21e
CodingManoj <mvp2612@hotmail.com>
2023-10-04 22:51:06 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "aws_spot_instance_request" "spot" {
   instance_type             = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment      = true 
   vpc_security_group_ids    = [aws_security_group.allows_app.id]
+  subnet_id                 = element(data.terraform_remote_state.vpc.output.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
     Name = "${var.COMPONENT}=${var.ENV}"
@@ -14,12 +15,15 @@ resource "aws_spot_instance_request" "spot" {
 
 # Creates OD Instance 
 resource "aws_instance" "od" {
-  count             = var.OD_INSTANCE_COUNT
-  ami               = data.aws_ami.ami.image_id 
-  instance_type     = var.OD_INSTANCE_TYPE
-  vpc_security_group_ids    = [aws_security_group.allows_app.id]
+  count                         = var.OD_INSTANCE_COUNT
+  ami                           = data.aws_ami.ami.image_id 
+  instance_type                 = var.OD_INSTANCE_TYPE
+  vpc_security_group_ids        = [aws_security_group.allows_app.id]
+  subnet_id                     = element(data.terraform_remote_state.vpc.output.PRIVATE_SUBNET_IDS, count.index)
 
   tags = {
     Name = "${var.COMPONENT}=${var.ENV}"
   }
 }
+
+# If this is called by frontend component, then these instances has to be created in the PUBLIC SUBNET. If not, they are supposed to be created on PRIVATE SUBNET.
