349c09745d36961bdcd63a37b65d54fc8e45c26f
CodingManoj <mvp2612@hotmail.com>
2023-10-04 22:31:40 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 # Creates SPOT Instances 
 
 resource "aws_spot_instance_request" "spot" {
+  count                 = var.SPOT_INSTANCE_COUNT
+
   ami                   = data.aws_ami.ami.image_id
   spot_price            = "0.03"
   instance_type         = var.SPOT_INSTANCE_TYPE
--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
 variable "ENV" {}
 variable "COMPONENT" {}
 variable "SPOT_INSTANCE_TYPE" {}
+variable "SPOT_INSTANCE_COUNT" {}
