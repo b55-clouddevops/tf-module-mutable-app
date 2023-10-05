dd347ca5261ebd800e08afd584bf44d984601649
CodingManoj <mvp2612@hotmail.com>
2023-10-04 22:42:50 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 # Creates SPOT Instances 
 resource "aws_spot_instance_request" "spot" {
-  count                 = var.SPOT_INSTANCE_COUNT
+  count                     = var.SPOT_INSTANCE_COUNT
 
-  ami                   = data.aws_ami.ami.image_id
-  instance_type         = var.SPOT_INSTANCE_TYPE
-  wait_for_fulfillment  = true 
+  ami                       = data.aws_ami.ami.image_id
+  instance_type             = var.SPOT_INSTANCE_TYPE
+  wait_for_fulfillment      = true 
+  vpc_security_group_ids    = [aws_security_group.allows_app.id]
 
   tags = {
     Name = "${var.COMPONENT}=${var.ENV}"
@@ -14,8 +15,9 @@ resource "aws_spot_instance_request" "spot" {
 # Creates OD Instance 
 resource "aws_instance" "od" {
   count             = var.OD_INSTANCE_COUNT
-  ami               = data.aws_ami.ami.image_id
+  ami               = data.aws_ami.ami.image_id 
   instance_type     = var.OD_INSTANCE_TYPE
+  vpc_security_group_ids    = [aws_security_group.allows_app.id]
 
   tags = {
     Name = "${var.COMPONENT}=${var.ENV}"
--- vars.tf
+++ vars.tf
@@ -5,3 +5,5 @@ variable "SPOT_INSTANCE_COUNT" {}
 
 variable "OD_INSTANCE_TYPE" {}
 variable "OD_INSTANCE_COUNT" {}
+
+variable "APP_PORT" {}
