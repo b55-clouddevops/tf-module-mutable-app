f185e5cc13dcd01494872051dd7d71e3d12ffca8
CodingManoj <mvp2612@hotmail.com>
2023-10-04 22:36:38 -0400

    Git Auto Push

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,23 @@
 # Creates SPOT Instances 
-
 resource "aws_spot_instance_request" "spot" {
   count                 = var.SPOT_INSTANCE_COUNT
 
   ami                   = data.aws_ami.ami.image_id
-  spot_price            = "0.03"
   instance_type         = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment  = true 
 
   tags = {
-    Name = "CheapWorker"
+    Name = "${var.COMPONENT}=${var.ENV}"
+  }
+}
+
+# Creates OD Instance 
+resource "aws_instance" "web" {
+  count             = var.OD_INSTANCE_COUNT
+  ami               = data.aws_ami.ami.image_id
+  instance_type     = var.OD_INSTANCE_TYPE
+
+  tags = {
+    Name = "${var.COMPONENT}=${var.ENV}"
   }
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,6 @@ variable "ENV" {}
 variable "COMPONENT" {}
 variable "SPOT_INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
+
+variable "OD_INSTANCE_TYPE" {}
+variable "OD_INSTANCE_COUNT" {}
