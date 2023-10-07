116ea6523eab2df8c540323c385ccf9cca04e072
CodingManoj <mvp2612@hotmail.com>
2023-10-06 22:26:16 -0400

    Git Auto Push

--- app-deploy.tf
+++ app-deploy.tf
@@ -3,7 +3,6 @@ resource "null_resource" "app" {
   count               = local.INSTANCE_COUNT
 
   connection {
-    type     = "ssh"
     user     = local.SSH_USERNAME
     password = local.SSH_PASSWORD
     host     = element(local.INSTANCE_PRIVATE_IPS, count.index)
