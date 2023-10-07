21bb6022f6ac8181f145bd47fcf6ed66a1ef0f1c
CodingManoj <mvp2612@hotmail.com>
2023-10-06 23:17:02 -0400

    Git Auto Push

--- listerner.tf
+++ listerner.tf
@@ -7,12 +7,7 @@ resource "aws_lb_listener" "private" {
   protocol          = "HTTP"
 
   default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
   }
 }
