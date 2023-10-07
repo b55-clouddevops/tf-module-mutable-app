72933f34461610ede5aabc25cbd5142536e99169
CodingManoj <mvp2612@hotmail.com>
2023-10-06 23:26:19 -0400

    Git Auto Push

--- listerner.tf
+++ listerner.tf
@@ -11,3 +11,22 @@ resource "aws_lb_listener" "private" {
     target_group_arn = aws_lb_target_group.app.arn
   }
 }
+
+#  Creates a rule in the private loadBalancer listener
+resource "aws_lb_listener_rule" "app_rule" {
+  count             = var.INTERNAL ? 1 : 0
+
+  listener_arn      = aws_lb_listener.private.arn
+  priority          = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.data.terraform_remote_state.vpc.PRIVATE_HOSTED_ZONE_NAME "]
+    }
+  }
+}
