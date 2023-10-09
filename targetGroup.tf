4be9e1d06c928b36c70f089a39f5176147e26ea7
CodingManoj <mvp2612@hotmail.com>
2023-10-08 23:06:39 -0400

    Git Auto Push

--- targetGroup.tf
+++ targetGroup.tf
@@ -4,6 +4,16 @@ resource "aws_lb_target_group" "app" {
   port              = 8080
   protocol          = "HTTP"
   vpc_id            = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  health_check {
+    enabled             = true 
+    healthy_threshold   = 2 
+    interval            = 5 
+    timeout             = 4
+    path                = "/health"
+    unhealthy_threshold = 2
+
+  }
 }
 
 # Attaches the target group to the loadBalancer 
@@ -11,5 +21,5 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   count             = local.INSTANCE_COUNT
   target_group_arn  = aws_lb_target_group.app.arn
   target_id         = element(local.INSTANCE_IDS, count.index)
-  port              = 8080
+  port              = var.APP_PORT
 }
