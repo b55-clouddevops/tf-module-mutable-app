60173e59ac9080cb35ea7d3990008b7d2cc72e6f
CodingManoj <mvp2612@hotmail.com>
2023-10-08 22:53:50 -0400

    Git Auto Push

--- listerner_rule.tf
+++ listerner_rule.tf
@@ -9,7 +9,7 @@ resource "random_integer" "priority" {
 resource "aws_lb_listener_rule" "app_rule" {
   count             = var.INTERNAL ? 1 : 0
 
-  listener_arn      = aws_lb_listener.private.*.arn[0]
+  listener_arn      = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority          = random_integer.priority.result
 
   action {
