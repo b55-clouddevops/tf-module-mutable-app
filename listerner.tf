0ea2443171b99df0dae0e6151196389ee789b4d1
CodingManoj <mvp2612@hotmail.com>
2023-10-08 22:37:21 -0400

    Git Auto Push

--- listerner.tf
+++ listerner.tf
@@ -1,16 +1,4 @@
-# This creates the listener and adds to the private ALB
-resource "aws_lb_listener" "private" {
-  count             = var.INTERNAL ? 1 : 0
 
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN 
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
 
 
 # Generates a Random Number in the given range 
